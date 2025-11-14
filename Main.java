efc72879ddf256daa329ff8c4d922c9fc782f04c
Ajay Avasi <79114792+AjayAvasi@users.noreply.github.com>
2025-11-14 13:37:34 -0500

    Create Square Array method
    Created square array method which puts random numbers in a square array.

---
+++ Main.java
@@ -0,0 +1,27 @@
+public class Main
+{
+	public static int[][] squareArray(int size)
+	{
+		int[][] arr = new int[size][size];
+		for(int i = 0; i < size; i++)
+		{
+			for(int j = 0; j < size; j++)
+			{
+				arr[i][j] = (int)(Math.random() * 10) + 1;
+			}
+		}
+		return arr;
+	}
+	public static void main(String[] args)
+	{
+		int[][] arr = squareArray(3);
+		for(int i = 0; i < 3; i++)
+		{
+			for(int j = 0; j < 3; j++)
+			{
+				System.out.print(arr[i][j] + " ");
+			}
+			System.out.println();
+		}
+	}
+}
